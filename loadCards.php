07f8e3abfb1c77390e386babe0cd7a16838f844d
Lukas Bühler <lukasmc6@gmail.com>
2018-02-18 20:07:38 +0100

    php test

--- loadCards.php
+++ loadCards.php
@@ -25,7 +25,7 @@ if ($result->num_rows > 0) {
         array_push($rows, json_encode($row));
     }
 
-    echo json_encode($rows);
+    echo "{" . join(",", $rows) . "}";
 
 } else {
     echo "{error: '0 results'}";
