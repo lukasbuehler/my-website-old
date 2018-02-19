65e47be7034e4116eac4b95b45928398db1029cf
Lukas Bühler <lukasmc6@gmail.com>
2018-02-19 22:31:29 +0100

    fixed php

--- loadCards.php
+++ loadCards.php
@@ -22,7 +22,7 @@ if ($result->num_rows > 0) {
     // output data of each row
 
     while($row = $result->fetch_assoc()) {
-        $rows[] = json_encode($row);
+        $rows[] = json_encode(utf8_encode($row));
     }
 
     echo "[" . join(",", $rows) . "]";
